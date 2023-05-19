b89b62e3898e636b91dab5f8eb08888cb8bf6292
akil-tajwar <akiltajwar@gmail.com>
2023-05-19 17:06:10 +0600

    added category json file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const toy = require('./data/toy.json')
+const category = require('./data/category.json')
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,9 @@ app.get('/', (req, res) => {
 
 app.get('/toy', (req, res) => {
     res.send(toy);
+})
+app.get('/category', (req, res) => {
+    res.send(category);
 })
 app.listen(port, () => {
     console.log(`toy car is running on prot: ${port}`);
